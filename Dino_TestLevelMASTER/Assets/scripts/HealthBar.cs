761be26b95b632b8d3ef3e2f6e0d4ec510c8319c
Brandon <d4rthtyran0us@gmail.com>
2014-11-20 15:18:52 -0500

    updated health/scripts
    health bar adjusts to screen resolution.

---
+++ Dino_TestLevelMASTER/Assets/scripts/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour 
+{
+	public Texture2D frame;
+	public Rect framePosition;
+
+	public Texture2D healthBar;
+	public Rect HealthBarPosition;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		framePosition.x = (Screen.width - framePosition.width) / 2;
+		framePosition.width = Screen.width * 0.39f;
+		framePosition.height = Screen.height * 0.049f;
+		GUI.DrawTexture (framePosition, frame);
+	}
+}
