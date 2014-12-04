5291fb7986e0bf7575cdb5f97d88f110daa9e4d6
Brandon <d4rthtyran0us@gmail.com>
2014-12-04 16:12:01 -0500

    working on health bar
    making the bar adjust to damage taken. changed lighting as well.

--- Dino_TestLevelMASTER/Assets/scripts/HealthBar.cs
+++ Dino_TestLevelMASTER/Assets/scripts/HealthBar.cs
@@ -6,6 +6,10 @@ public class HealthBar : MonoBehaviour
 	public Texture2D frame;
 	public Rect framePosition;
 
+	public float horizontalDistance;
+	public float verticleDistance;
+	public float width;
+	public float height;
 	public Texture2D healthBar;
 	public Rect HealthBarPosition;
 
@@ -21,9 +25,26 @@ public class HealthBar : MonoBehaviour
 
 	void OnGUI()
 	{
+		drawFrame ();
+		drawBar ();
+	}
+
+	void drawFrame()
+	{
 		framePosition.x = (Screen.width - framePosition.width) / 2;
-		framePosition.width = Screen.width * 0.39f;
+		framePosition.width = Screen.width * 0.38f;
 		framePosition.height = Screen.height * 0.049f;
 		GUI.DrawTexture (framePosition, frame);
 	}
+
+	void drawBar()
+	{
+		HealthBarPosition.x = framePosition.x + framePosition.width * horizontalDistance;
+		HealthBarPosition.y = framePosition.y + framePosition.height * verticleDistance;
+		HealthBarPosition.width = framePosition.width * width;
+		HealthBarPosition.height = framePosition.height * height;
+
+		GUI.DrawTexture (HealthBarPosition, healthBar);
+	}
+
 }
